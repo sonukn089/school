c435f62e47bbb912d352230236024aa3cf1a4765
Sonu <sonukn089@gmail.com>
2018-06-15 00:18:55 +0530

    add something

--- application/views/configuration/class_configuration.php
+++ application/views/configuration/class_configuration.php
@@ -150,7 +150,6 @@
 				    	<button type="submit" name="action" value="delete" class="btn bg-primary">Delete</button><br><br>
 					</form>
 				<?php endforeach; ?>
-				
 				<div class="text-info bg-info">You can edit or delete stream by press 	
 					concern Button it sure that you have not created subject and classes depending edited or Deleted Stream.
 				</div>
